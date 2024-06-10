0b523245b9c789b60a52956cfe75796c63c7e7c4
Кристина Волосова <justanobody017527@gmail.com>
2024-06-10 14:51:03 +0300

    Prototype 1

---
+++ PrototypePattern/Source.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <memory>
+#define delimeter "\n------------------------------------------------\n\n"
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Player
+{
+public:
+	virtual Player* clone() const = 0;
+	/*Player(const std::string& name, int id) : name(name), id(id)
+	{
+
+	}*/
+	virtual ~Player()
+	{
+
+	}
+	virtual void print() const = 0;
+};
+class CarPlayer : public Player
+{
+	std::string name;
+	int id;
+public:
+	CarPlayer(const std::string& name, int id) : name(name), id(id)
+	{
+		cout << "Car ctor: " << this << endl;
+	}
+	~CarPlayer()
+	{
+		cout << "car dtor: " << this << endl;
+	}
+	void print() const override
+	{
+		cout << this << ":\t" << name << " " << id << endl;
+	}
+	Player* clone() const override
+	{
+		return new CarPlayer(*this);
+	}
+};
+class BikePlayer : public Player
+{
+	std::string name;
+	int id;
+public:
+	BikePlayer(const std::string& name, int id) : name(name), id(id)
+	{
+		cout << "Bike ctor: " << this << endl;
+	}
+	~BikePlayer()
+	{
+		cout << "Bike dtor: " << this << endl;
+	}
+	void print() const override
+	{
+		cout << this << ":\t" << name << " " << id << endl;
+	}
+	Player* clone() const override
+	{
+		return new BikePlayer(*this);
+	}
+};
+enum PlayerType
+{
+	CAR, BIKE
+};
+class PlayerFactory
+{
+	std::map<PlayerType, Player*> players;
+public:
+	PlayerFactory()
+	{
+		players[CAR] = new CarPlayer("BMW", 735);
+		players[BIKE] = new BikePlayer("Harley Davidson", 200);
+	}
+	~PlayerFactory()
+	{
+		delete players[CAR];
+		delete players[BIKE];
+	}
+	Player* createPlayer(PlayerType type)
+	{
+		return players[type]->clone();
+	}
+};
+//#define PROBLEM
+#define SOLUTION
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef PROBLEM
+	const Player car_player_template("Car player", 100);
+	const Player bike_player_template("Bike player", 200);
+	car_player_template.print();
+	bike_player_template.print();
+	Player car_player = car_player_template;
+	car_player.print();
+	Player bike_player = bike_player_template;
+	bike_player.print();
+#endif // PROBLEM
+	PlayerFactory factory;
+	cout << delimeter;
+	Player* car_player = factory.createPlayer(CAR);
+	car_player->print();
+
+	cout << delimeter;
+	Player* bike_player = factory.createPlayer(BIKE);
+	bike_player->print();
+	cout << delimeter;
+	delete bike_player;
+	delete car_player;
+	cout << delimeter;
+}
